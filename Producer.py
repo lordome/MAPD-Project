3e6647e6e08bfe3c4e51e547ef30550bdc3fd9b4
root <root@mapd-b-gr09-1.novalocal>
2021-07-04 17:05:48 +0200

    "Updated project"

---
+++ Producer.py
@@ -0,0 +1,29 @@
+from kafka.admin import KafkaAdminClient, NewTopic
+import os
+import ssl
+import json
+import time
+import pandas as pd
+from kafka import KafkaProducer
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+KAFKA_BOOTSTRAP_SERVERS = 'localhost:9092'
+kafka_admin = KafkaAdminClient(
+    bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
+)
+
+producer = KafkaProducer(bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS)
+
+for i in range(0, 81):
+    i = str(i).zfill(2)
+    url = f"https://cloud-areapd.pd.infn.it:5210/swift/v1/AUTH_d2e941ce4b324467b6b3d467a923a9bc/MAPD_miniDT_stream/data_0000{i}.txt"
+    df = pd.read_csv(url)
+    print(df)
+    for j in range(0, df.shape[0]):
+        jj = df.iloc[j].to_dict()
+        for key in ['HEAD', 'FPGA', "TDC_CHANNEL", "ORBIT_CNT", "BX_COUNTER"]:
+            jj[key] = int(jj[key])
+        producer.send('topic_stream', json.dumps(jj).encode('utf-8'))
+        time.sleep(0.0004)
+    producer.flush()
