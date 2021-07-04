b58dfa4b280da40b3db82440d8b1bf8b0e5d8b6c
root <root@mapd-b-gr09-1.novalocal>
2021-07-04 18:13:36 +0200

    First working version

--- Producer.py
+++ Producer.py
@@ -8,7 +8,7 @@ from kafka import KafkaProducer
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-KAFKA_BOOTSTRAP_SERVERS = 'localhost:9092'
+KAFKA_BOOTSTRAP_SERVERS = '10.67.22.100:9092'
 kafka_admin = KafkaAdminClient(
     bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
 )
@@ -19,7 +19,6 @@ for i in range(0, 81):
     i = str(i).zfill(2)
     url = f"https://cloud-areapd.pd.infn.it:5210/swift/v1/AUTH_d2e941ce4b324467b6b3d467a923a9bc/MAPD_miniDT_stream/data_0000{i}.txt"
     df = pd.read_csv(url)
-    print(df)
     for j in range(0, df.shape[0]):
         jj = df.iloc[j].to_dict()
         for key in ['HEAD', 'FPGA', "TDC_CHANNEL", "ORBIT_CNT", "BX_COUNTER"]:
