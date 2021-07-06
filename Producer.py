2a92a6bc4ef1170c7be60654dada7ff7a3be633a
root <root@mapd-b-gr09-1.novalocal>
2021-07-06 17:09:59 +0200

    Added cleaning procedure in producer

--- Producer.py
+++ Producer.py
@@ -10,7 +10,7 @@ import pandas as pd
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-KAFKA_BOOTSTRAP_SERVERS = '10.67.22.100:9092'
+KAFKA_BOOTSTRAP_SERVERS = '10.67.22.226:9092'
 kafka_admin = KafkaAdminClient(
     bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
 )
@@ -21,6 +21,7 @@ for i in range(0, 81):
     i = str(i).zfill(2)
     url = f"https://cloud-areapd.pd.infn.it:5210/swift/v1/AUTH_d2e941ce4b324467b6b3d467a923a9bc/MAPD_miniDT_stream/data_0000{i}.txt"
     df = pd.read_csv(url)
+    df = df[df.ORBIT_CNT < 5e8]
     for j in range(0, df.shape[0]):
         jj = df.iloc[j].to_dict()
         for key in ['HEAD', 'FPGA', "TDC_CHANNEL", "ORBIT_CNT", "BX_COUNTER"]:
